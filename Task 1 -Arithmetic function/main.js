d98fb1d75d319989979d22ac6bab2c04e52068b4
Ramani Shankar <ramshankarjan1222@gmail.com>
2022-07-07 00:33:15 +0530

    TASK-1  is completed

---
+++ Task 1 -Arithmetic function/main.js
@@ -0,0 +1,16 @@
+a=5;
+b=25;
+c=a+b;
+d=50;
+e=10;
+f=d-e;
+g=10;
+h=10;
+i=g*h;
+k=50;
+l=10;
+m=k/l;
+console.log("Addition:   ",+c)
+console.log("Subtraction:   ",+f)
+console.log("Multiplication:   ",+i)
+console.log("Division:   ",+m)
